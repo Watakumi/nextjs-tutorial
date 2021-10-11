4a6892bde8c7a514dcc94bc13449e8272b95c2d7
石渡拓海 <t-ishiwata@sight-visit.com>
2021-10-11 18:20:26 +0900

    Finish next js tutorial

---
+++ pages/posts/first-post.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+
+import Image from 'next/image'
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpeg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+)
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+          <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <YourComponent/>
+    </>
+  )
+}
